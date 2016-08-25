7fa40cdb6e3f378ab58c0371da149149f2440c5c
YangMing <wandererparsifal@gmail.com>
2016-08-26 01:37:34 +0800

    optimized algorithm

--- app/src/main/java/com/parsifal/log01/presenter/StatisticsPresenterImpl.java
+++ app/src/main/java/com/parsifal/log01/presenter/StatisticsPresenterImpl.java
@@ -1,5 +1,7 @@
 package com.parsifal.log01.presenter;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.parsifal.log01.bean.TimeAndPlace;
 import com.parsifal.log01.utils.FileUtil;
@@ -33,31 +35,58 @@ public class StatisticsPresenterImpl implements StatisticsPresenter {
     @Override
     public void loadFromFile() {
         String json = mFileUtil.load();
+        Log.e(TAG, json);
         TimeAndPlace[] timeAndPlaces = new Gson().fromJson(json, TimeAndPlace[].class);
         int length = timeAndPlaces.length;
-        LinkedList<String> sl1 = new LinkedList<String>();
-        LinkedList<String> sl2 = new LinkedList<String>();
+        LinkedList<Calendar> calendars = new LinkedList<Calendar>();
+
+        LinkedList<String> list_work = new LinkedList<String>();
+        LinkedList<String> list_home = new LinkedList<String>();
         TimeAndPlace timeAndPlace = null;
+        int day_before = 0;
         for (int i = 0; i < length; i++) {
             timeAndPlace = timeAndPlaces[i];
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(timeAndPlace.date);
-            int hour = calendar.get(Calendar.HOUR_OF_DAY);
-            if (12 > hour) {
-                sl1.add(mDateFormat.format(calendar.getTime()));
+            day_before = calendar.get(Calendar.DAY_OF_MONTH);
+            if (0 == calendars.size() || calendars.getLast().get(Calendar.DAY_OF_MONTH) == day_before) {
+                calendars.add(calendar);
             } else {
-                sl2.add(mDateFormat.format(calendar.getTime()));
+                Calendar first = calendars.getFirst();
+                int hour_first = first.get(Calendar.HOUR_OF_DAY);
+                if (12 > hour_first) {
+                    list_work.add(mDateFormat.format(first.getTime()));
+                }
+                Calendar last = calendars.getLast();
+                int hour_last = last.get(Calendar.HOUR_OF_DAY);
+                if (12 <= hour_last) {
+                    list_home.add(mDateFormat.format(last.getTime()));
+                }
+                calendars.clear();
+                calendars.add(calendar);
+            }
+            if (i == length - 1) {
+                Calendar first = calendars.getFirst();
+                int hour_first = first.get(Calendar.HOUR_OF_DAY);
+                if (12 > hour_first) {
+                    list_work.add(mDateFormat.format(first.getTime()));
+                }
+                Calendar last = calendars.getLast();
+                int hour_last = last.get(Calendar.HOUR_OF_DAY);
+                if (12 <= hour_last) {
+                    list_home.add(mDateFormat.format(last.getTime()));
+                }
             }
         }
         String[] samples1 = null;
         String[] samples2 = null;
-        if (0 != sl1.size()) {
-            samples1 = new String[sl1.size()];
-            samples1 = sl1.toArray(samples1);
+        if (0 != list_work.size()) {
+            samples1 = new String[list_work.size()];
+            samples1 = list_work.toArray(samples1);
         }
-        if (0 != sl2.size()) {
-            samples2 = new String[sl2.size()];
-            samples2 = sl2.toArray(samples2);
+        if (0 != list_home.size()) {
+            samples2 = new String[list_home.size()];
+            samples2 = list_home.toArray(samples2);
         }
         mStatisticsView.drawGraphics(samples1, samples2);
     }
