392bc12c98cbbafac89b0637aa8758417bce6953
yangming <wandererparsifal@gmail.com>
2016-09-01 12:03:36 +0800

    add Application

---
+++ app/src/main/java/com/parsifal/log01/LogApplication.java
@@ -0,0 +1,14 @@
+package com.parsifal.log01;
+
+import android.app.Application;
+
+/**
+ * Created by YangMing on 2016/9/1 12:02.
+ */
+public class LogApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
